1fe95946b03abdd95d47e011fd850c3de9a770be
ninomack <miha_ushakov@mail.ru>
2020-07-03 01:19:14 +1000

    Add TabBar controller

---
+++ MusicPlayer/MainTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabBarController.swift
+//  MusicPlayer
+//
+//  Created by Михаил Ушаков on 03.07.2020.
+//  Copyright © 2020 Михаил Ушаков. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+    
+}
