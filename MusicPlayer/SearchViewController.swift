e9480f5f407ee1a531df1bda0645d06d860def73
ninomack <miha_ushakov@mail.ru>
2020-07-07 19:27:57 +1000

    lesson 2

--- MusicPlayer/SearchViewController.swift
+++ MusicPlayer/SearchViewController.swift
@@ -9,10 +9,39 @@
 import Foundation
 import UIKit
 
+struct TrackModel {
+    var trackName: String
+    var artistName: String
+}
+
 class SearchViewController: UITableViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    let tracks = [TrackModel(trackName: "track 1", artistName: "artist 1"), TrackModel(trackName: "track 2", artistName: "artist 2")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        setupSearchBar()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ident")
 
+    }
+    
+    private func setupSearchBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return tracks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ident", for: indexPath)
+        let track = tracks[indexPath.row]
+        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        return cell
     }
 }
